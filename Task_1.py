4231e4f335381a206a21a8fd20c8c6a93c3b53af
Chirag-g11 <chiraggoyal1102@gmail.com>
2025-10-07 21:28:00 +0530

    Updated code or fixed issue

---
+++ Task_1.py
@@ -0,0 +1,14 @@
+#  Task 1 : To Perform various operations
+
+a=input('Enter First number :')
+b=input('Enter second number :')
+a=int(a)
+b=int(b)
+Addition= a+b
+print('Addition :',Addition)
+Sub=a-b
+print('Subtraction :',Sub)
+multiplication=a*b
+print('Multiplication :',multiplication)
+division=a/b
+print('Division :',division)
---
+++ Task_2.py
@@ -0,0 +1,6 @@
+# To print greeting by taking name as input
+
+f_name=input('Enter your first name :')
+l_name=input('Enter your last name :')
+name=f_name+" "+l_name
+print('Hello, ',name,"!",' Welcome to Python program.')
